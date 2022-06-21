3550476608ddec56327fb61298ba1ade1ba5ffe9
右究 <aisowe@sina.com>
2022-06-21 21:16:11 +0800

    feat: 改为generator函数

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
 import './style.css'
 
 function* fn(items) {
-  let base = items % 5 || 5
+  for (let item = 1; item <= items; item++) {
+    let base = item % 5 || 5
 
-  for (let i = 0; i < 5; i++) {
-    yield items
-  }
-
-  if (base !== 1) {
-    for (let j = 0; j < 5; j++) {
-      for (let i = base; i > 0; i--) {
-        yield items - i + 1
-      }
+    for (let i = 0; i < 5; i++) {
+      yield item
     }
-  }
 
-  let res = items
-  if (base === 5) {
-    let level = 5
-
-    do {
-      res = Math.floor(items / level)
-      const flag = res % 5
-      if (flag !== 0) {
-        const a = flag * level
-        for (let j = 0; j < 5; j++) {
-          for (let i = a; i > 0; i--) {
-            yield items - i + 1
-          }
+    if (base !== 1 && base !== 5) {
+      for (let j = 0; j < 5; j++) {
+        for (let i = base; i > 0; i--) {
+          yield item - i + 1
         }
       }
+    }
 
-      level = level * 5
-    } while (res > 0)
+    let res = item
+    if (base === 5) {
+      let level = 5
+
+      do {
+        res = Math.floor(item / level)
+        const flag = res % 5
+        if (flag !== 0) {
+          const a = flag * level
+          for (let j = 0; j < 5; j++) {
+            for (let i = a; i > 0; i--) {
+              yield item - i + 1
+            }
+          }
+        }
+
+        level = level * 5
+      } while (res > 0)
+    }
   }
 }
 
+const run = fn(125)
 const list = []
-for (let j = 0; j < 1000; j++) {
-  const run = fn(j + 1)
-  for (let i of run) {
-    list.push(i)
-  }
+for (let i of run) {
+  list.push(i)
 }
 
-document.querySelector('#app').innerHTML = list.join(', ')
-console.log(list.length)
+document.querySelector('#app').innerText = list.join(', ')
